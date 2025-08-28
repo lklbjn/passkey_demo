95093dadcd114bcdc9c49e744f99ab8e361a86a1
lklbjn <sudo_i@126.com>
2025-08-28 14:13:00 +0800

    feat(config): 通过配置文件管理 passkey 相关配置
    - 在 application-dev.yaml 中添加 passkey 相关配置
    - 新增 PasskeyProperties 类用于读取配置文件
    - 修改 PasskeyConfig 类，使用 PasskeyProperties 中的配置

--- backend/src/main/java/io/github/lklbjn/passkey/passkeydemo/config/PasskeyConfig.java
+++ backend/src/main/java/io/github/lklbjn/passkey/passkeydemo/config/PasskeyConfig.java
@@ -3,14 +3,12 @@ package io.github.lklbjn.passkey.passkeydemo.config;
 import com.yubico.webauthn.RelyingParty;
 import com.yubico.webauthn.data.RelyingPartyIdentity;
 import io.github.lklbjn.passkey.passkeydemo.service.WebAuthnCredentialRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashSet;
-import java.util.List;
 
 /**
  * @author lklbjn
@@ -18,21 +16,23 @@ import java.util.List;
  */
 @Slf4j
 @Configuration
+@AllArgsConstructor
 @ComponentScan("io.github.lklbjn.passkey.passkeydemo")
 @MapperScan("io.github.lklbjn.passkey.passkeydemo.mapper")
 public class PasskeyConfig {
+
+    private PasskeyProperties properties;
 
     // 核心服务类
     @Bean
     public RelyingParty relyingParty(WebAuthnCredentialRepository credentialRepo) {
         return RelyingParty.builder()
                 .identity(RelyingPartyIdentity.builder()
-                        // .id("passkey.ciallo.io")
-                        .id("localhost")
-                        .name("PassKeyDemo")
+                        .id(properties.getId())
+                        .name(properties.getName())
                         .build())
                 .credentialRepository(credentialRepo)
-                .origins(new HashSet<>(List.of("http://localhost:8080","http://localhost:8193"))) // 允许的前端源
+                .origins(properties.getOrigins()) // 允许的前端源
                 .build();
     }
 
---
+++ backend/src/main/java/io/github/lklbjn/passkey/passkeydemo/config/PasskeyProperties.java
@@ -0,0 +1,19 @@
+package io.github.lklbjn.passkey.passkeydemo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
+
+/**
+ * @author lklbjn
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "passkey")
+public class PasskeyProperties {
+    private String id;
+    private String name;
+    private Set<String> origins;
+}
